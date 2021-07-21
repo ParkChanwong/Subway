ac42014dcdfd582da51be2c21d729779f26ffe73
신호윤 <shinhoyoon1013@gmail.com>
2021-07-22 01:38:02 +0900

    subwayHowToUse.js tab ing

--- js/subwayHowToUse.js
+++ js/subwayHowToUse.js
@@ -1,1 +1,15 @@
-$('.single-item').slick();
+//$('.single-item').slick();
+
+function storeOrder(){
+    $('#store_order').css("display", "block");
+    $('#order_tip').css("display", "none");
+    $('.tab_storeOrder').addClass('active');
+    $('.tab_orderTip').removeClass('active');
+}
+
+function orderTip(){
+    $('#store_order').css("display", "none");
+    $('#order_tip').css("display", "block");
+    $('.tab_orderTip').removeClass('active');
+    $('.tab_storeOrder').addClass('active');
+}
